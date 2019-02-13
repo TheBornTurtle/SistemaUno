75d9298d55496eee24b40a53f89498eee5e3d492
Louis Hollingworth <bornturtle@outlook.com>
2019-02-13 19:11:19 +0000

    Initial commit Added Employee Basics

---
+++ src/main/kotlin/co/aplicared/kt/su/SU.kt
@@ -0,0 +1,4 @@
+package co.aplicared.kt.su
+
+class SU {
+}
---
+++ src/main/kotlin/co/aplicared/kt/su/util/Employee.kt
@@ -0,0 +1,12 @@
+package co.aplicared.kt.su.util
+
+class Employee {
+    private var id: Int? = null
+    private var  name: String? = null
+    private var company: String = "OmniTech"
+    private var ongoing: Array<String>? = null
+
+    fun Employee(id: Int, name: String, company: String, ongoing:Array<String>) {
+
+    }
+}
